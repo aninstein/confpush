a6154995306581f68b01f18685d5658efc1855b4
lichangan <lica1@chinatelecom.cn>
2023-01-15 22:15:54 +0800

    springboot init

---
+++ confpush/src/main/java/com/github/confpush/ConfpushApplication.java
@@ -0,0 +1,13 @@
+package com.github.confpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfpushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfpushApplication.class, args);
+	}
+
+}
